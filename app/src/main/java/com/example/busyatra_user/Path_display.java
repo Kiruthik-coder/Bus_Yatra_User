a34f91b9edb32597aabd581ddcea5eef308a1469
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-08-11 10:59:59 +0530

    test

---
+++ app/src/main/java/com/example/busyatra_user/Path_display.java
@@ -0,0 +1,81 @@
+package com.example.busyatra_user;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
+
+public class Path_display extends Fragment {
+    private GoogleMap mMap;
+    private LatLng startLatLng = new LatLng(12.9717, 79.1380); // Start coordinates (San Francisco, CA)
+    private LatLng endLatLng = new LatLng(12.9692, 79.1559);
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+//            LatLng sydney = new LatLng(-34, 151);
+//            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            mMap.addMarker(new MarkerOptions().position(startLatLng).title("Start"));
+            mMap.addMarker(new MarkerOptions().position(endLatLng).title("End"));
+
+            // Move camera to show both markers
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(startLatLng);
+            builder.include(endLatLng);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 100));
+
+            // Draw polyline between start and end points
+            PolylineOptions polylineOptions = new PolylineOptions()
+                    .add(startLatLng)
+                    .add(endLatLng);
+            Polyline polyline = mMap.addPolyline(polylineOptions);
+        }
+
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_path_display, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
